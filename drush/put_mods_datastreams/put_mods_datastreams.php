d4bad3639079ef640639ba7fc85d6a3ffd06c191
Kristina Spurgin <kristina@is-prod-migration.lyrtech.org>
2020-03-03 15:10:28 -0500

    Add progress bar; Correct README

--- drush/put_mods_datastreams/put_mods_datastreams.php
+++ drush/put_mods_datastreams/put_mods_datastreams.php
@@ -1,7 +1,7 @@
 <?php
 
 // // // VARIABLES TO SET
-$modsdir = '/opt/migrations/ns/coll/mods-current';
+$modsdir = '/opt/migrations/client/dir_containing_mods';
 // // // END OF VARIABLES TO SET
 
 drupal_static_reset('islandora_get_tuque_connection');
@@ -9,6 +9,8 @@ $tuque = islandora_get_tuque_connection();
 $repository = $tuque->repository;
 
 $modslist = scandir($modsdir);
+$modscount = count($modslist);
+$counter = 0;
 
 foreach ($modslist as $mods) {
     if (stripos($mods, '.xml') !== false) {
@@ -20,8 +22,14 @@ foreach ($modslist as $mods) {
         $obj = islandora_object_load($pid);
         $mds = $obj['MODS'];
         $mds->setContentFromFile($path);
-        print 'Put ' . $path . "\n";
+        $counter = ++$counter;
+        echo progress_bar($counter, $modscount, 'MODS Replacement Status');
     }
 }
 
+function progress_bar($done, $total, $info="", $width=50) {
+    $perc = round(($done * 100) / $total);
+    $bar = round(($width * $perc) / 100);
+    return sprintf("%s%%[%s>%s]%s\r", $perc, str_repeat("=", $bar), str_repeat(" ", $width-$bar), $info);
+}
 ?>
